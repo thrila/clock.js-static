7d184f834e8a232a70ea17635b03ec3a082537e7
thrila <ifeanyichukwundubuizu@gmail.com>
2022-10-05 12:34:54 +0100

    Build - static clock

---
+++ script.js
@@ -0,0 +1,5 @@
+var date = new Date();
+var day = document.getElementById('d').innerHTML = date.getDate();
+var hours = document.getElementById('h').innerHTML =date.getHours();
+var minutes = document.getElementById('min').innerHTML = date.getMinutes();
+var seconds = document.getElementById('sec').innerHTML = date.getSeconds();
